05b5a26ceddd190905d4920f2dd639994e066479
Tomas_Erlebach <t.erleb@seznam.cz>
2022-01-11 20:52:29 +0100

    add jest

---
+++ test/chatController.test.js
@@ -0,0 +1,12 @@
+﻿const controller = require('../controller/chatController');
+
+test('Ověření hesla', async () => {
+    let hash = controller.hash("heslo");
+    expect(controller.compare("heslo", hash)).toBe(true);
+});
+
+
+test('chybné heslo', async () => {
+    let hash = controller.hash("heslo");   
+    expect(controller.compare("veslo", hash)).toBe(false);
+});
